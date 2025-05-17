4cf62b8c192f32077bf59d903e89fe093ca42948
Nguyen_Dang_Shinnraa <nguyenminhdang552k4@gmail.com>
2025-05-18 05:46:54 +0700

    sendata

--- src/main/java/com/example/Web_ban_sach/controller/UserController.java
+++ src/main/java/com/example/Web_ban_sach/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.example.Web_ban_sach.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.Web_ban_sach.domain.User;
 import com.example.Web_ban_sach.service.UserService;
+import org.springframework.ui.Model;
 
 @Controller
 public class UserController {
@@ -14,7 +19,27 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        String test = this.userService.handle();
+        model.addAttribute("eric", test);
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @GetMapping("/admin/user/create1")
+    public String getUserCreatePage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User user) {
+        System.out.println("runhere" + user);
         return "hello";
     }
 }
---
+++ src/main/java/com/example/Web_ban_sach/domain/User.java
@@ -0,0 +1,65 @@
+package com.example.Web_ban_sach.domain;
+
+public class User {
+    private long id;
+    private String email;
+    private String password;
+    private String name;
+    private String phone;
+    private String address;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", email=" + email + ", password=" + password + ", name=" + name + ", phone=" + phone
+                + ", address=" + address + "]";
+    }
+
+}
